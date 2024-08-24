8f5cd92b54f41bca65dfbbeb29dee294ffa3cde1
Eduard <eddzuba@gmail.com>
2024-08-24 22:52:41 +0400

    + pastE event

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -10,19 +10,38 @@ import {CommonModule} from "@angular/common";
   imports: [CommonModule, DragDropModule ]
 })
 export class ScoreboardComponent  {
-  score1 = 1;
+  score1 = 0;
   score2 = 0;
+  currentSound: number = 0; // номер счета в произношении счета
+
+  playScore1 = 0; // число которое нужно проиграть первым
+  playScore2 = 0; // число которое нужно проиграть вторым
+
+  private audio: HTMLAudioElement;
 
   constructor() {
-
+    this.audio = new Audio();
+    // Добавляем обработчик события `ended`
+    this.audio.addEventListener('ended', () => {
+      console.log('Audio playback finished.');
+      if( this.currentSound == 0) {
+        this.currentSound = 1;
+        this.playScore(this.playScore2);
+      } else {
+        this.currentSound = 0;
+      }
+      // Здесь можно выполнить дополнительные действия после завершения аудио
+    });
   }
 
   incrementScore1() {
     this.score1++;
+    this.playSound(1);
   }
 
   incrementScore2() {
     this.score2++;
+    this.playSound(2);
   }
 
   resetScores() {
@@ -40,4 +59,20 @@ export class ScoreboardComponent  {
 
   }
 
+  private playSound(number: number) {
+    if(number == 1) {
+      this.playScore1 = this.score1;
+      this.playScore2 = this.score2;
+    } else {
+      this.playScore1 = this.score1;
+      this.playScore2 = this.score2;
+    }
+    this.playScore(this.playScore1);
+
+  }
+
+  private playScore(number: number) {
+    this.audio.src = `audio/${number}.wav`;
+    this.audio.play();
+  }
 }
