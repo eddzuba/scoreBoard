778f363d74013b9341dfebfab5d4c53954dc7c06
Eduard <eddzuba@gmail.com>
2024-08-25 01:34:36 +0400

    ДОбавил звоков

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -10,27 +10,54 @@ import {CommonModule} from "@angular/common";
   imports: [CommonModule, DragDropModule ]
 })
 export class ScoreboardComponent  {
-  score1 = 0;
-  score2 = 0;
+  score1 = 23;
+  score2 = 20;
   currentSound: number = 0; // номер счета в произношении счета
 
   playScore1 = 0; // число которое нужно проиграть первым
   playScore2 = 0; // число которое нужно проиграть вторым
+  isControlBall: boolean = false; //Это контрольный мяч
+  matchOver:boolean = false; // Матч завершен
 
   private audio: HTMLAudioElement;
+
 
   constructor() {
     this.audio = new Audio();
     // Добавляем обработчик события `ended`
     this.audio.addEventListener('ended', () => {
-      console.log('Audio playback finished.');
-      if( this.currentSound == 0) {
-        this.currentSound = 1;
-        this.playScore(this.playScore2);
-      } else {
-        this.currentSound = 0;
+      if(this.matchOver) {
+        return;
       }
-      // Здесь можно выполнить дополнительные действия после завершения аудио
+      if(this.isControlBall){
+        this.isControlBall = false;
+        return;
+      }
+
+      if(this.currentSound == 1){
+        this.currentSound = 0;
+        this.isControlBall = this.isControlBallScore()
+        if (this.isControlBall) {
+          this.audio.src = `audio/controlball.wav`;
+          this.audio.play();
+          return;
+        }
+
+        this.matchOver = this.isMatchOver();
+        if(this.isMatchOver()) {
+          this.audio.src = `audio/win.wav`;
+          this.audio.play();
+          return;
+        }
+        return;
+      }
+
+      if(this.currentSound == 0){
+        this.playScore(this.playScore2);
+        this.currentSound = 1;
+        return;
+      }
+
     });
   }
 
@@ -64,8 +91,8 @@ export class ScoreboardComponent  {
       this.playScore1 = this.score1;
       this.playScore2 = this.score2;
     } else {
-      this.playScore1 = this.score1;
-      this.playScore2 = this.score2;
+      this.playScore1 = this.score2;
+      this.playScore2 = this.score1;
     }
     this.playScore(this.playScore1);
 
@@ -74,5 +101,35 @@ export class ScoreboardComponent  {
   private playScore(number: number) {
     this.audio.src = `audio/${number}.wav`;
     this.audio.play();
+  }
+
+  private isControlBallScore() {
+    const maxScore = 25;
+
+    // Check if one of the teams is at 24 points and the other has less than 24
+    if ((this.score1 === 24 && this.score2 < 24) || (this.score2 === 24 && this.score1 < 24)) {
+      return true;
+    }
+
+    // Check if one of the teams can win with the next point in a deuce situation
+    if ((this.score1 >= maxScore - 1 || this.score2 >= maxScore - 1)
+      && Math.abs(this.score1 - this.score2) === 1) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private isMatchOver(): boolean {
+    const winningScore = 25;
+    const minDifference = 2;
+
+    // Check if one of the teams has reached the winning score and has at least a 2-point lead
+    if ((this.score1 >= winningScore || this.score2 >= winningScore)
+      && Math.abs(this.score1 - this.score2) >= minDifference) {
+      return true;
+    }
+
+    return false;
   }
 }
